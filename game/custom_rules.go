3165e9316b331e7b4cd660b886f005104469543c
filip-dob <filip.dobrzycki@gmail.com>
2026-07-11 16:17:04 -0700

    Updates rules and scoring

--- game/custom_rules.go
+++ game/custom_rules.go
@@ -13,38 +13,36 @@ type Rule struct {
 // All rules from the 2022 game that carry point penalties.
 // @formatter:off
 var rules = []*Rule{
-	{1, "G206", false, true, "A team or ALLIANCE may not collude with another team to each purposefully violate a rule in an attempt to influence Ranking Points."},
-	{2, "G210", true, false, "A strategy not consistent with standard gameplay and clearly aimed at forcing the opponent ALLIANCE to violate a rule is not in the spirit of FIRST Robotics Competition and not allowed."},
-	{3, "G301", true, false, "A DRIVE TEAM member may not cause significant delays to the start of their MATCH."},
-	{4, "G401", false, false, "In AUTO, each DRIVE TEAM member must remain in their staged areas. A DRIVE TEAM member staged behind a HUMAN STARTING LINE may not contact anything in front of that HUMAN STARTING LINE, unless for personal or equipment safety, to press the E-Stop or A-Stop, or granted permission by a Head REFEREE or FTA."},
-	{5, "G402", false, false, "In AUTO, a DRIVE TEAM member may not directly or indirectly interact with a ROBOT or an OPERATOR CONSOLE unless for personal safety, OPERATOR CONSOLE safety, or pressing an E-Stop or A-Stop. A HUMAN PLAYER entering FUEL onto the FIELD is an exception to this rule."},
-	{6, "G403", true, false, "In AUTO, a ROBOT whose BUMPERS are completely across the CENTER LINE (i.e. to the opposite side of the CENTER LINE from its ROBOT STARTING LINE) may not contact an opponent ROBOT."},
-	{7, "G404", true, false, "A ROBOT may not deliberately use a SCORING ELEMENT in an attempt to ease or amplify a challenge associated with a FIELD element."},
-	{8, "G405", false, false, "A ROBOT may not intentionally eject SCORING ELEMENTS from the FIELD (either directly or by bouncing off a FIELD element or other ROBOT) with an exception of through the opening at the base of the OUTPOST."},
-	{9, "G405", true, false, "A ROBOT may not intentionally eject SCORING ELEMENTS from the FIELD (either directly or by bouncing off a FIELD element or other ROBOT) with an exception of through the opening at the base of the OUTPOST."},
-	{10, "G406", true, false, "Neither a ROBOT nor a HUMAN PLAYER may damage a SCORING ELEMENT."},
-	{11, "G407", true, false, "A ROBOT may not launch a SCORING ELEMENT into their HUB unless their BUMPERS are partially or fully within their ALLIANCE ZONE."},
-	{12, "G408", false, false, "A ROBOT may not do either of the following with FUEL released by the HUB unless and until that FUEL contacts anything else besides that ROBOT or FUEL CONTROLLED by that ROBOT: A. gain greater than MOMENTARY CONTROL of FUEL, or B. push or redirect FUEL to a desired location or in a preferred direction."},
-	{13, "G408", true, false, "A ROBOT may not do either of the following with FUEL released by the HUB unless and until that FUEL contacts anything else besides that ROBOT or FUEL CONTROLLED by that ROBOT: A. gain greater than MOMENTARY CONTROL of FUEL, or B. push or redirect FUEL to a desired location or in a preferred direction."},
-	{14, "G410", false, false, "ROBOT extensions may not interact with the carpet, BUMPS, or TOWER BASE such that the BUMPERS are lifted out of the BUMPER ZONE."},
-	{15, "G412", true, false, "A ROBOT is prohibited from the following interactions with FIELD elements (with the exception of the RUNGS and UPRIGHTS): grabbing, grasping, attaching to, becoming entangled with, suspending from."},
-	{16, "G413", true, false, "A ROBOT may not extend beyond any of the horizontal or vertical expansion limits described in R105, R106, and R107."},
-	{17, "G415", true, false, "A ROBOT with BUMPERS completely outside of their ALLIANCE ZONE may not damage or functionally impair an opponent ROBOT by initiating contact, either directly or transitively via a SCORING ELEMENT CONTROLLED by the ROBOT: A. inside the vertical projection of an opponent’s ROBOT PERIMETER, or B. with the opponent’s BUMPER backing or mounting."},
-	{18, "G416", true, false, "A ROBOT may not intentionally and/or recklessly damage or functionally impair an opponent ROBOT."},
-	{19, "G417", true, false, "A ROBOT may not deliberately attach to, tip over, or entangle with an opponent ROBOT."},
-	{20, "G418", false, false, "A ROBOT may not PIN an opponent’s ROBOT for more than 3 seconds."},
-	{21, "G418", true, false, "A ROBOT may not PIN an opponent’s ROBOT for more than 3 seconds."},
-	{22, "G419", true, false, "2 or more ROBOTS that appear to a REFEREE to be working together may not isolate or close off any major element of MATCH play."},
-	{23, "G420", true, false, "A ROBOT may not contact, directly or transitively through a SCORING ELEMENT, an opponent ROBOT in contact with an opponent TOWER during the last 30 seconds of the MATCH regardless of who initiates contact."},
-	{24, "G421", false, false, "A DRIVE TEAM member must remain in their designated area as follows: A. DRIVERS and COACHES may not contact anything outside their ALLIANCE AREA, B. a DRIVER must use the OPERATOR CONSOLE in the DRIVER STATION to which they are assigned, as indicated on the team sign, C. a HUMAN PLAYER may not contact anything outside their ALLIANCE AREA, and D. a TECHNICIAN may not contact anything outside their designated area."},
-	{25, "G422", true, false, "A ROBOT shall be operated only by the DRIVERS and/or HUMAN PLAYERS of that team. A COACH activating their E-Stop or A-Stop is the exception to this rule."},
-	{26, "G423", false, false, "A DRIVE TEAM member may not extend: A. into the CHUTE beyond the ALLIANCE-colored tape line while the CHUTE DOOR is open, or B. into the CORRAL beyond the ALLIANCE-colored tape line."},
-	{27, "G424", true, false, "A DRIVE TEAM member may not deliberately use a SCORING ELEMENT in an attempt to ease or amplify a challenge associated with a FIELD element."},
-	{28, "G425", true, false, "FUEL may only be introduced to the FIELD by a HUMAN PLAYER or DRIVER in the following ways: A. through the CHUTE, B. through the bottom opening in the OUTPOST, or C. thrown over the top of the ALLIANCE WALL from the OUTPOST AREA."},
-	{29, "G426", false, false, "DRIVE COACHES may not touch SCORING ELEMENTS, unless for safety purposes."},
-	{30, "G427", false, false, "Off-FIELD FUEL may only be stored in the CHUTE and the CORRAL. Excess FUEL, defined as the CHUTE & CORRAL being full, must immediately be entered onto the FIELD."},
-	{31, "G427", true, false, "Off-FIELD FUEL may only be stored in the CHUTE and the CORRAL. Excess FUEL, defined as the CHUTE & CORRAL being full, must immediately be entered onto the FIELD."},
-}
+	
+	{1, "G210", true, false, "A strategy aimed at forcing an opponent to violate a rule is not allowed."},
+	{2, "G401", true, false, "In AUTO, each DRIVE TEAM member must remain in their staged areas. A DRIVE TEAM member staged behind a HUMAN STARTING LINE may not contact anything in front of that HUMAN STARTING LINE, unless for personal or equipment safety, to press the E-Stop or A-Stop, or granted permission by a Head REFEREE or FTA."},
+	{3, "G402", true, false, "In AUTO, a DRIVE TEAM member may not directly or indirectly interact with a ROBOT or an OPERATOR CONSOLE unless for personal safety, OPERATOR CONSOLE safety, or pressing an E-Stop or A-Stop."},
+	{4, "G403", false, false, "A ROBOT may not cross the midfield line during auton."},
+	{5, "G408", true, false, "Neither a ROBOT nor a HUMAN PLAYER may damage a treasure."},
+	{6, "G415", true, false, "A ROBOT may not contact another robot inside of its perimeter."},
+	{7, "G424", true, false, "A ROBOT may not deliberately attach to, tip, or entangle with an opponent ROBOT."},
+	{8, "G425", false, false, "A ROBOT may not PIN an opponent's ROBOT for more than 3 seconds."},
+	{9, "G429", true, false, "A DRIVE TEAM member must remain in their designated area as follows: A. DRIVERS and COACHES may not contact anything outside their ALLIANCE AREA, B. a DRIVER must use the OPERATOR CONSOLE in the DRIVER STATION to which they are assigned, as indicated on the team sign, C. a HUMAN PLAYER may not contact anything outside their ALLIANCE AREA, and D. a TECHNICIAN may not contact anything outside their designated area."},
+	{10, "G430", true, false, "A ROBOT shall be operated only by the DRIVERS."},
+	{11, "G434", true, false, "COACHES may not touch CANNONBALLS, unless for safety purposes."},
+	{12, "MA2601", true, true, "Contacting the opposing teams balance beam at during the endgame [Ref Discretion, Can stack with MA2602]. This results in an automatic endgame RP for the other team, no matter the scoring."},
+	{12, "MA2602", true, true, "Contacting the opposing team AT ALL while they’re in their own Balance Beam 5 seconds after endgame begins [Can stack with MA2601]. This results in an automatic endgame RP for the other team, no matter the scoring."},
+	{13, "MA2603", true, true, "Going into another team's safe house within auton. This also results in an automatic auto RP for the other team, no matter the scoring. "},
+	{14, "MA2604", true, true, "No Hoarding: More than 6 non-scoring treasures in your safe house. Major Foul is given at the beginning of infraction, given every 10 seconds 6 non-scoring treasures are in the safe house."},
+	{15, "MA2606", false, false, "MA2606 Violation: Entering another alliance’s Human Player Loading zone, Safe House, or touching the balance beam before endgame. This can be reapplied every 10 seconds if contact does not end."},
+	{16, "MA2607", false, false, "Placing/Throwing a treasure into the field not through the Human Player Loading holes [doesn’t apply during the “toss”, may be escalated by the head ref if flagrant]"},
+	{17, "MA2608", false, false, "Placing/Throwing a treasure that bounces not first on the same alliance’s robot, or the Human Player Loading zone Floor."},
+	{18, "MA2609", false, false, "Contacting scoring own shelf repeatably or in a unsafe manner"},
+	{19, "MA2610", false, false, "Starting with over 1 piece during auton. The robot is also ineligible to gain points within auton by scoring in the shelf"},
+	{20, "MA2611", false, false, "Descoring pieces from other teams [can stack with MA2620]"},
+	{21, "MA2612", false, false, "Robot deliberately permanently destroying a treasure "},
+	{22, "MA2613", false, false, "Adding treasures within endgame [treasure does not count for scoring]"},
+	{23, "MA2614", false, false, "Placing/Shooting treasures within the opposing alliance zones"},
+	{24, "MA2616", false, false, "Controlling more than 3 gamepieces at a non-momentarily time [additional treasures do not count for scoring][ref discretion]"},
+	{25, "MA2617", false, false, "Throwing a treasure into/on the shelf not inside own Safe House[treasure does not count for scoring]"}
+ }
+	
+
 
 // @formatter:on
 var ruleMap map[int]*Rule
